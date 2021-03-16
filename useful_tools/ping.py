1b19b617ac1030922d824d81f921eb1b0eaf997f
Hawkry Zhang <hawkrybruin@gmail.com>
2021-03-15 22:37:27 -0700

    Update ping.py according to the changes made to ping function.

--- useful_tools/ping.py
+++ useful_tools/ping.py
@@ -1,13 +1,15 @@
 from pybear import Manager
 
 # bear = Manager.BEAR(port="/dev/UB02B", baudrate=8000000)
-bear = Manager.BEAR(port="COM18", baudrate=8000000)
+bear = Manager.BEAR(port="COM7", baudrate=8000000)
 bear_list = []
 found = False
-for i in range(0, 8):
+start_ID = 0  # The ID to start the search with.
+end_ID = 8  # The ID to end the search with
+for i in range(start_ID, end_ID+1):
     m_id = i
     print("Pinging BEAR with ID %d" % m_id)
-    data = bear.ping(m_id)
+    data = bear.ping(m_id)[0]
     if data:
         print("Found BEAR with ID %d." % m_id)
         found = True
